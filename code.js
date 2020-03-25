1c31be69462a30af46f727cdefd848661b80b4eb
Maria Gonzalez <mayigd0502@gmail.com>
2020-03-25 12:10:45 -0500

    Crear lista de paises

---
+++ code.js
@@ -0,0 +1,7 @@
+var paises = [
+    "Armenia","Moldavia","Estonia","Letonia","Lituania","Georgia","Azerbaiyan","Tayikistan"
+    ,"Kirguistan","Bielorusia","Uzbekistan","Turkmenistan","Ucrania","Kazajistan","Rusia"
+];
+var estadosSatelites = [
+    "Albania","Alemania Oriental","Bulgaria","Checoslovaquia","Hungria","Polonia","Rumania"
+]
