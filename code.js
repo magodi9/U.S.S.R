a5f5034fbd0c72bf72916118111b6de6db3afc79
Maria Gonzalez <mayigd0502@gmail.com>
2020-03-31 19:45:27 -0500

    Desaparecer menu y aparecer guion por cada letra

--- code.js
+++ code.js
@@ -1,7 +1,35 @@
-var paises = [
-    "Armenia","Moldavia","Estonia","Letonia","Lituania","Georgia","Azerbaiyan","Tayikistan"
-    ,"Kirguistan","Bielorusia","Uzbekistan","Turkmenistan","Ucrania","Kazajistan","Rusia"
+var paises = ["Albania", "Alemania Oriental", "Bulgaria", "Checoslovaquia", "Hungria", "Polonia", "Rumania"
+    , "Armenia", "Moldavia", "Estonia", "Letonia", "Lituania", "Georgia", "Azerbaiyan", "Tayikistan"
+    , "Kirguistan", "Bielorusia", "Uzbekistan", "Turkmenistan", "Ucrania", "Kazajistan", "Rusia"
 ];
-var estadosSatelites = [
-    "Albania","Alemania Oriental","Bulgaria","Checoslovaquia","Hungria","Polonia","Rumania"
-];
+
+
+// 
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        enJuego: false,
+        letras: [],
+        seleccion: []
+    },
+    methods: {
+        iniciarJ: function () {
+            this.enJuego = true
+            var paises_alteatorio = Math.floor(Math.random() * (paises.length));
+            this.seleccion = paises[paises_alteatorio]
+            for (i = 0; i < this.seleccion.length; i++) {
+                this.letras.push("___");
+            }
+
+
+
+        }
+
+
+        //Solo me compartio VsCode...
+
+    }
+
+});
